df94673022382d9ddc5bd378c6d7df6cd3fabd12
Ronny Alvarado <rsalvarado777@gmail.com>
2019-06-19 21:13:19 -0500

    MVP COMPLETE - Created unit test for the fail function

--- enhancing/enhancer.spec.js
+++ enhancing/enhancer.spec.js
@@ -54,4 +54,47 @@ describe("the enhancer", () => {
       expect(successWeapon3).toEqual(successItem3);
     });
   });
+
+  describe("the fail function", () => {
+    it("should decrease item durability if enhancement less than 15, decrease item durability by 10 if enhancement is 15 or more, and should decrease enhancement if enhancement is greater than 16", () => {
+      const weapon4 = {
+        name: "dagger",
+        durability: 50,
+        enhancement: 10
+      };
+      const weapon5 = {
+        name: "bow",
+        durability: 70,
+        enhancement: 20
+      };
+      const weapon6 = {
+        name: "sword",
+        durability: 80,
+        enhancement: 15
+      };
+      const failItem4 = {
+        name: "dagger",
+        durability: 45,
+        enhancement: 10
+      };
+      const failItem5 = {
+        name: "bow",
+        durability: 60,
+        enhancement: 19
+      };
+      const failItem6 = {
+        name: "sword",
+        durability: 70,
+        enhancement: 15
+      };
+
+      const failWeapon4 = enhancer.fail(weapon4);
+      const failWeapon5 = enhancer.fail(weapon5);
+      const failWeapon6 = enhancer.fail(weapon6);
+
+      expect(failWeapon4).toEqual(failItem4);
+      expect(failWeapon5).toEqual(failItem5);
+      expect(failWeapon6).toEqual(failItem6);
+    });
+  });
 });
